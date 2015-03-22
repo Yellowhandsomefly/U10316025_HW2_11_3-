41c2ff3359459cc37f092c1f1876f93a5ac83a84
Yellowhandsomefly <I279KenEmily22@gmail.com>
2015-03-22 22:38:59 +0800

    打錯題目要求
    支票帳戶要有一個透支上限 讓他 在透支上限內都可以提錢

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -2,21 +2,20 @@ public class CheckingAccount extends Account{
 
 	/**data field*/
 	private double withdrawMoney;
+	private double savingMoney;
 
 	/**a no-arg constructor to initialize data field*/
 	CheckingAccount(){
 
 		super();	
-		withdrawMoney = 0;
+		
 	}
 
 	/**a constructor to give the value to data field*/
-	CheckingAccount(int iid, double bbalance, double wwithdrawMoney){
+	CheckingAccount(int iid, double bbalance){
 
 		super(iid, bbalance);
-		this.withdrawMoney = wwithdrawMoney;
-		withdrawMoney = overdraft(withdrawMoney);
-		super.withdraw(withdrawMoney);
+		
 	}
 	
 	/**a method to invoke setAnnualInterestRate method of the superclass to set annual interest rate*/
@@ -25,38 +24,40 @@ public class CheckingAccount extends Account{
 		super.setAnnualInterestRate(annualInterestRate);
 	}
 	
-	/**a set method to set withdraw money*/		
-	public void setWithdrawMoney(double wwithdrawMoney){
-	
-		this.withdrawMoney = wwithdrawMoney;
-		withdrawMoney = overdraft(withdrawMoney);
-		super.withdraw(withdrawMoney);
-	}
-	
-	/**a get method to return withdraw money*/		
-	public double getWithdrawMoney(){
+	/**a method to withdraw and record the new balance*/
+	public void withdraw(double withdraw){
 
-		return withdrawMoney;
+		double money;
+		money = overdraft(withdraw);
+		super.withdraw(money);
 	}
 
+	/**a method to deposit and record the new balance*/
+	public void deposit(double deposit){
+	
+		super.deposit(deposit);
+	}
+	
 	/**decide if the account has the overdraft limit*/
 	public double overdraft(double withdrawMoney){
 
 		double balance = super.getBalance();
 		
-		if(balance < withdrawMoney){
+		if(balance - withdrawMoney < -5000){
 
-			System.out.println("Your balance is less than withdraw Money");
+			System.out.println("Your balance is less $5000 than withdraw Money");
 		
 			withdrawMoney = 0;
 		}
-
+		
+		System.out.println("Withdraw Money : " + withdrawMoney);
 		return withdrawMoney;
 	}
 	
 	@Override
 	public String toString(){
 
-		return "---------------------------------------\nWithdraw Money : " + getWithdrawMoney() + "\n" + super.toString();
+		return super.toString();
 	}
 }
+	

0f9bac84c7fea017c492b09741ec9acc9a0f5dae
Yellowhandsomefly <I279KenEmily22@gmail.com>
2015-03-22 15:05:22 +0800

    註解
    說名程式各個功能

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,13 +1,16 @@
 public class SavingsAccount extends Account{
 
+	/**data field*/
 	private double savingMoney;
 
+	/**a no-arg constructor to initialize data field*/
 	SavingsAccount(){
 
 		super();	
 		savingMoney = 0;
 	}
 
+	/**a constructor to give the value to data field*/
 	SavingsAccount(int iid, double bbalance, double ssavingMoney){
 
 		super(iid, bbalance);
@@ -15,17 +18,20 @@ public class SavingsAccount extends Account{
 		super.deposit(savingMoney);
 	}
 	
+	/**a method to invoke setAnnualInterestRate method of the superclass to set annual interest rate*/
 	public void setAnnualInterestRate(double annualInterestRate){
 
 		super.setAnnualInterestRate(annualInterestRate);
 	}
 	
+	/**a set method to set saving money*/
 	public void setSavingMoney(double ssavingMoney){
 	
 		this.savingMoney = ssavingMoney;
 		super.deposit(savingMoney);
 	}
 	
+	/**a get method to return saving money*/
 	public double getSavingMoney(){
 
 		return savingMoney;
